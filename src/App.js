81bc38dc26eaf92dce36387a0bf866857e547fba
Aaron Craig <aaron@10etechnology.com>
2017-08-11 15:44:18 -0700

    getting started

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { AppBar} from 'material-ui';
-import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from '../reducers';
 
 injectTapEventPlugin();
 
+const store = createStore(reducer);
+
 const App = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-    <AppBar title="A demo app for js.la"/>
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <AppBar title="A demo app for js.la"/>
+    </MuiThemeProvider>
+  </Provider>
 );
 
 export default App;
